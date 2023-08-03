c474739bc36851107265668e308a49f63d492d60
MarlaJahari <sciencefan19@gmail.com>
2023-08-04 04:39:30 +0530

    added init xyz algorithm + deps

--- src/testxyz1-package.cpp
+++ src/testxyz1-package.cpp
@@ -7,7 +7,7 @@ IntegerVector findIndex(NumericVector array, double value) {
 
   for (int i = 0; i < array.size(); ++i) {
     if (array[i] == value) {
-      indices.push_back(i); // Convert 0-based to 1-based index
+      indices.push_back(i);
     }
   }
 
@@ -18,7 +18,7 @@ IntegerVector findIndex(NumericVector array, double value) {
 NumericVector generate_uniform_values(int n, double a = 0.0, double b = 1.0) {
   NumericVector result(n);
 
-  // Generate random values from U[a, b]
+  // generate random values from U[a, b]
   for (int i = 0; i < n; ++i) {
     result[i] = R::runif(a, b);
   }
@@ -30,12 +30,12 @@ NumericVector generate_uniform_values(int n, double a = 0.0, double b = 1.0) {
 
 // [[Rcpp::export]]
 NumericVector generate_random_projection(int n, int M, bool with_replacement) {
-  NumericVector D = Rcpp::runif(M); // Generate vector D with independent components U[0, 1]
+  NumericVector D = Rcpp::runif(M); // Generate vector D with independent components from U[0, 1]
 
   NumericVector R(n); // Initialize vector R with zeros
   NumericVector indices1(M);
   IntegerVector v = seq(1,n);
-  // Sampling with/o replacement
+  // sampling
   indices1 = Rcpp::sample(v, M, with_replacement);
 
   for (int i = 0; i < M; ++i) {
@@ -44,7 +44,7 @@ NumericVector generate_random_projection(int n, int M, bool with_replacement) {
 
   for (int i = 0; i < n; ++i) {
     IntegerVector ind = findIndex(indices1, i);
-    float sum1 = 0.0; // Declare and initialize sum1 here
+    float sum1 = 0.0; // declare and initialize sum1
     if(ind.size()==1){
       R[i]=D[ind[0]];
     }
@@ -61,10 +61,12 @@ NumericVector generate_random_projection(int n, int M, bool with_replacement) {
 
 
 //[[Rcpp::export]]
-IntegerMatrix equalpairs(NumericVector u, NumericVector v, IntegerVector ou, IntegerVector ov, int max_number_of_pairs) {
+IntegerMatrix equal_pairs(NumericVector u, NumericVector v, int max_number_of_pairs) {
   //set sizes of array
   int nu = u.size();
   int nv = v.size();
+  IntegerVector ou=seq(0,nu-1);
+  IntegerVector ov=seq(0,nv-1);
 
   //init two lists to store pairs
   std::list<int> pairs_u;
@@ -103,13 +105,13 @@ IntegerMatrix equalpairs(NumericVector u, NumericVector v, IntegerVector ou, Int
         }
       }
     }
-    //if there are too many pairs kill the search
+    // too many pairs kill the search
     if(count > max_number_of_pairs) {
       break;
     }
   }
   int n = 0;
-  //fill pairs in a 2x|pairs| matrix
+  //fill pairs in a 2x(pairs) matrix
   if(pairs_u.size() > 0) {
     IntegerMatrix pairs(2,pairs_u.size());
     while(!pairs_u.empty()) {
@@ -131,7 +133,6 @@ IntegerMatrix equalpairs(NumericVector u, NumericVector v, IntegerVector ou, Int
 double interaction_strength(NumericMatrix X, NumericVector Y, int j, int k) {
   int n = X.nrow();
   int p = X.ncol();
-  int count = 0;
   double sum = 0.0;
 
   // calculate Z(n x p) matrix
@@ -155,3 +156,74 @@ double interaction_strength(NumericMatrix X, NumericVector Y, int j, int k) {
 }
 
 
+// [[Rcpp::export]]
+NumericMatrix push(IntegerMatrix M, IntegerVector new_vector) {
+  if (M.nrow() != new_vector.size()) {
+    Rcpp::stop("Dimensions of the matrix and vector do not agree.");
+  }
+
+  int nrow = M.nrow();
+  int ncol = M.ncol();
+
+  NumericMatrix new_matrix(nrow + 1, ncol);
+
+  // copy elements from the old matrix to the new matrix
+  for (int i = 0; i < nrow; ++i) {
+    for (int j = 0; j < ncol; ++j) {
+      new_matrix(i, j) = M(i, j);
+    }
+  }
+
+  // append the new vector as the last row of the new matrix
+  for (int j = 0; j < ncol; ++j) {
+    new_matrix(nrow, j) = new_vector[j];
+  }
+
+  return new_matrix;
+}
+
+
+// function implementing final xyz
+// [[Rcpp::export]]
+IntegerMatrix strongest_pairs(NumericMatrix X, NumericVector Y, int M, int L, int gamma) {
+  int n = X.nrow();
+  int p = X.ncol();
+  NumericMatrix Z(n,p);
+  if(p!=Y.size()){
+    stop("Inconsistency in Dimensions, come back later");
+  }
+  for (int i=0; i<n; ++i) {
+    for (int l=0; l < p;++l) {
+      Z(i,l) = Y[i]*X(i, l);
+    }
+  }
+  IntegerMatrix I(2,0);
+  for (int i=0;i<L;++i){
+    NumericVector R=generate_random_projection(n, M, 0);
+    NumericVector x=transpose(X)*R;
+    NumericVector z=transpose(Z)*R;
+    IntegerMatrix eq=equal_pairs(x,z,50);
+    for(int i=0;i < eq.ncol();++i){
+
+      if(interaction_strength(X,Y,eq(_,i)[0],eq(_,i)[1])>gamma){
+      push(I,eq(_,i));
+      }
+
+    }
+  }
+
+  return I;
+
+
+}
+
+// [[Rcpp::export]]
+NumericMatrix generateMatrix(int n, int p) {
+  NumericMatrix matrix(n, p);
+  return matrix;
+}
+// [[Rcpp::export]]
+NumericVector go(int n) {
+  NumericVector vec(n);
+  return vec;
+}
