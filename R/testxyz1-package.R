6e3e90050c4337810d5414a7f881aa0224bbd857
MarlaJahari <sciencefan19@gmail.com>
2023-08-20 23:05:05 +0530

    added R func getpairs

--- R/testxyz1-package.R
+++ R/testxyz1-package.R
@@ -7,3 +7,15 @@ NULL
 #' @useDynLib testxyz1, .registration = TRUE
 ## usethis namespace: end
 NULL
+
+#given a list of cartesian products, returns all pairs
+getpairs <- function(p) {
+  mat<-matrix(0,0,2)
+  for (i in seq_along(p)) {
+    grid<-(expand.grid(p[[i]][[1]],p[[i]][[2]]))
+    grid<- (as.matrix(grid))
+    mat<- rbind(mat,grid)
+    colnames(mat) <- NULL
+  }
+  return(t(mat))
+}
